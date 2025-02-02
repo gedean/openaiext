a766846228714a069d80de0d4fc2d8a62b80bef7
Gedean Dias <gedean.dias@gmail.com>
2025-02-02 04:25:57 -0300

    Rename O1 model constants to more generic reasoning model names
    - Update O1_BASIC_MODEL to BASIC_REASONING_MODEL
    - Update O1_ADVANCED_MODEL to ADVANCED_REASONING_MODEL
    - Adjust MODEL_MAP keys to use 'reasoning_basic' and 'reasoning_advanced'

--- lib/openaiext/model.rb
+++ lib/openaiext/model.rb
@@ -4,15 +4,15 @@ module OpenAIExt
     GPT_ADVANCED_MODEL        = ENV.fetch('OPENAI_GPT_ADVANCED_MODEL', 'gpt-4o')
     GPT_ADVANCED_MODEL_LATEST = ENV.fetch('OPENAI_GPT_ADVANCED_MODEL_LATEST', 'chatgpt-4o-latest')
 
-    O1_BASIC_MODEL            = ENV.fetch('OPENAI_O1_BASIC_MODEL', 'o1-mini')
-    O1_ADVANCED_MODEL         = ENV.fetch('OPENAI_O1_ADVANCED_MODEL', 'o1-preview')
+    BASIC_REASONING_MODEL     = ENV.fetch('OPENAI_BASIC_REASONING_MODEL', 'o1-mini')
+    ADVANCED_REASONING_MODEL  = ENV.fetch('OPENAI_ADVANCED_REASONING_MODEL', 'o1-preview')
 
     MODEL_MAP = {
       gpt_basic:           GPT_BASIC_MODEL,
       gpt_advanced:        GPT_ADVANCED_MODEL,
       gpt_advanced_latest: GPT_ADVANCED_MODEL_LATEST,
-      o1_basic:            O1_BASIC_MODEL,
-      o1_advanced:         O1_ADVANCED_MODEL
+      reasoning_basic:     BASIC_REASONING_MODEL,
+      reasoning_advanced:  ADVANCED_REASONING_MODEL
     }.freeze
 
     def self.select(model)
